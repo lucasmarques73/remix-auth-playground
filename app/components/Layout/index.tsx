e64c0f463fe5074509eb02d933598b6989f105db
Lucas Marques <lucasmarques73@hotmail.com>
2022-04-04 11:24:11 -0300

    Add is Auth in Layout

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -3,12 +3,13 @@ import Header from "../Header";
 
 interface LayoutProps {
   children?: React.ReactNode;
+  isAuthenticated: boolean;
 }
 
-export function Layout({ children }: LayoutProps) {
+export function Layout({ children, isAuthenticated }: LayoutProps) {
   return (
     <div className="flex flex-grow flex-col">
-      <Header isAuthenticated={false} />
+      <Header isAuthenticated={isAuthenticated} />
       <main className="container mx-auto flex-grow px-4">{children}</main>
       <Footer />
     </div>
