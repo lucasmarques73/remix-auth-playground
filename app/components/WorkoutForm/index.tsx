4072fcc720602a60f80c9507e7b3a56896779efc
Lucas Marques <lucasmarques73@hotmail.com>
2022-04-27 20:59:26 -0300

    refactor to manage serie

--- app/components/WorkoutForm/index.tsx
+++ app/components/WorkoutForm/index.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Form, Link } from "remix";
+import { v4 as uuidv4 } from "uuid";
+import { Serie } from "../Serie";
 
 export function WorkoutForm() {
+  const [qtdSerie, setQtdSerie] = useState(1);
+
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <Form method="post" className="w-full">
@@ -22,22 +27,49 @@ export function WorkoutForm() {
                   required
                 />
               </div>
-              <div className="mb-6 flex-grow">
-                <label
-                  htmlFor="workout-status"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Status do treino
-                </label>
-                <select
-                  id="workout-status"
-                  name="workout-status"
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:sky-700 focus:border-sky-700 block w-full p-2.5"
-                >
-                  <option>Em Construção</option>
-                  <option>Ativo</option>
-                  <option>Inativo</option>
-                </select>
+              <div className="mb-6 flex flex-row justify-between">
+                <div className="w-1/2 mr-2">
+                  <label
+                    htmlFor="workout-status"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Status do treino
+                  </label>
+                  <select
+                    id="workout-status"
+                    name="workout-status"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:sky-700 focus:border-sky-700 block w-full p-2.5"
+                  >
+                    <option>Em Construção</option>
+                    <option>Ativo</option>
+                    <option>Inativo</option>
+                  </select>
+                </div>
+                <div className="w-1/2 mr-2">
+                  <label
+                    htmlFor="workout-status"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Quantas séries por semana
+                  </label>
+                  <select
+                    id="workout-status"
+                    name="workout-status"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:sky-700 focus:border-sky-700 block w-full p-2.5"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setQtdSerie(Number(e.target.value));
+                    }}
+                  >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
+                    <option value={7}>7</option>
+                  </select>
+                </div>
               </div>
               <div className="mb-6 flex flex-row justify-between">
                 <div className="w-1/2 mr-2">
@@ -71,17 +103,22 @@ export function WorkoutForm() {
               </div>
             </div>
             <hr />
+            {[...Array(qtdSerie).keys()]
+              .map((x) => ++x)
+              .map((number) => {
+                return <Serie key={uuidv4()} number={number} />;
+              })}
           </div>
           <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
             <Link
               to="/app/workouts"
-              className="inline-flex mr-2 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700"
+              className="inline-flex mr-2 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-700 hover:bg-red-800"
             >
               Cancelar
             </Link>
             <button
               type="submit"
-              className="inline-flex ml-2 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-700 hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-700"
+              className="inline-flex ml-2 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-700 hover:bg-sky-800"
             >
               Salvar
             </button>
